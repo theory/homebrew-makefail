07d3a7753c39ae8a5921097c9b57aab729eaef29
David E. Wheeler <david@justatheory.com>
2018-09-06 10:02:28 -0400

    Add test case.

---
+++ Formula/makefail.rb
@@ -0,0 +1,13 @@
+class Makefail < Formula
+  desc "Testing case for Homebrew `make` bug"
+  version "1"
+  url "https://raw.githubusercontent.com/theory/homebrew-makefail/master/tryme.tgz"
+  sha256 "493bf653887db204f3063674fc581b79a2c286ad5e3c77f0d29a5b28808b7f50"
+  depends_on 'cpanminus' => :build
+  bottle :unneeded
+
+  def install
+    system *%W[cpanm --quiet --notest --local-lib-contained tryit List::MoreUtils::XS Exporter::Tiny];
+    system *%W[cpanm --verbose --notest --local-lib-contained tryit --installdeps --install-args -d .];
+  end
+end
